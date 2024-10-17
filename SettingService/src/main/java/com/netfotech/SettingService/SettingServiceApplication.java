70c99834de736880bbf528fd6433170a709fbdf9
VishalChaudhari25 <vishal@netfotech.in>
2024-10-17 16:59:10 +0530

    Setting service added

---
+++ SettingService/src/main/java/com/netfotech/SettingService/SettingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.netfotech.SettingService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SettingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SettingServiceApplication.class, args);
+	}
+
+}
---
+++ SettingService/src/test/java/com/netfotech/SettingService/SettingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netfotech.SettingService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SettingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
