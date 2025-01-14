7b6fc146993b7f636ae670685f6843daf287312c
VishalChaudhari25 <vishal@netfotech.in>
2025-01-14 12:10:15 +0530

    remove files

--- Employee-Setting/src/main/java/com/hrms/employeesetting/repository/EducationStandardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hrms.employeesetting.repository;
-
-import com.hrms.employeesetting.entity.EducationStandard;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EducationStandardRepository extends JpaRepository<EducationStandard, Long> {
-    // JpaRepository already provides common methods like findById, existsById, deleteById, etc.
-}
